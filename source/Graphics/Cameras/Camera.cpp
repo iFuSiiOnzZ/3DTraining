eae3ebc7a8be37c464a09c51307361703dfd8654
iFuSiiOnzZ <andreisystem@gmail.com>
2020-04-12 17:33:47 +0200

    Add LookAt function
    Former-commit-id: 8c603c1f0bae781bb0be4726afd593b1e209531d [formerly b666465d7bba67756e155042d00c7de9536f8be0]
    Former-commit-id: b77be81842d2a3d8f3f191d669652d588a1ad277

--- source/Graphics/Cameras/Camera.cpp
+++ source/Graphics/Cameras/Camera.cpp
@@ -18,3 +18,12 @@ CCamera::CCamera(float fov, float zNear, float zFar)
     m_Fov = fov;
     m_CameraType = ECameraType::UNDEFINED;
 }
+
+void CCamera::SetLookAt(const CVector3f &lookAt)
+{
+    CVector3f dir = (lookAt - m_CameraInfo.GetPosition());
+    float y = std::atan2f(dir.x, dir.z), p = std::atan2f(dir.y, std::sqrtf(dir.x * dir.x + dir.z * dir.z));
+
+    m_CameraInfo.SetYaw(y);
+    m_CameraInfo.SetPitch(p);
+}
