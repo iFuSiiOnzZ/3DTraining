86df4d1d16d927ba2dd5b287f80b2feaac97d421
iFuSiiOnzZ <andreisystem@gmail.com>
2020-04-13 12:02:48 +0200

    Add RenderQuad for DebugRendering
    Former-commit-id: 8e064765fbc045d49719f6dbcba945b88b9871cb [formerly 483e326700a659ceeb6ab881f48e34bc4a3a84cc]
    Former-commit-id: 7e390aadbd99c518c6cd13bf72b8a21b36945afc

--- source/Graphics/DebugRender/DebugRender.h
+++ source/Graphics/DebugRender/DebugRender.h
@@ -11,7 +11,9 @@ namespace CDebugRender
     void DrawLine       (const CVector4f &start, const CVector4f &end, const CVector4f &color);
     void DrawGrid       (int nLines, float sz, const CVector4f &color);
     void DrawAxis       (const CVector4f &origin, float sz);
+
     void DrawCube       (float x, float y, float z);
+    void RenderQuad     ();
 
     int TakeScreenShot  (const char *filename);
 
